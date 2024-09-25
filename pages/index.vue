99cf9a0c1f88924cb24fb83a21eed1914c255571
darkwebplayer <amrithanadhm@gmail.com>
2024-09-25 23:30:34 +0530

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useFlowbite } from "@/composables/flowbite";
+onMounted(() => {
+  useFlowbite(() => {
+    initFlowbite();
+  });
+});
+</script>
+
+<template>
+<HomeHero />
+<HomeCourses />
+<HomeEnroll />
+<HomeFooter />
+</template>
